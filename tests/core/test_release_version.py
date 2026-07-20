c86bb3e6c88750f93d6b465bc304bfdb41f5263b
Paolo Pietrelli <pablos3339@gmail.com>
2026-07-21 00:33:36 +0200

    Prepare Optees 0.9.1 release

--- tests/core/test_release_version.py
+++ tests/core/test_release_version.py
@@ -29,10 +29,10 @@ def test_release_version_rejects_unsupported_notation(value):
 
 
 def test_release_tag_cli_accepts_current_release(capsys):
-    assert main(["v0.9.0"]) == 0
-    assert "matches Optees 0.9.0" in capsys.readouterr().out
+    assert main(["v0.9.1"]) == 0
+    assert "matches Optees 0.9.1" in capsys.readouterr().out
 
 
 def test_release_tag_cli_rejects_mismatch(capsys):
-    assert main(["v0.9.0-rc.4"]) == 1
+    assert main(["v0.9.1-rc.1"]) == 1
     assert "does not match" in capsys.readouterr().err
